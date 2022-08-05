43861ecc59c0d5ffef3df16df1c00b364cea52e7
Maswdle <wondr009@163.com>
2022-08-05 22:18:04 +0800

    完善函数,插线板实现

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
+
+class Plugboard
+{
+public:
+    void Init(map<char, char> a)
+    {
+        for (pair<char, char> it: a)
+        {
+            int key = it.first >= 'a' && it.first <= 'z' ? it.first - 32 : it.first;
+            int value = it.second >= 'a' && it.second <= 'z' ? it.second - 32 : it.second;
+            this->pb[key] = value;
+
+        }
+    }
+
+    char getLetter(char p)
+    {
+        return p >= 'a' && p <= 'z' ? pb[p - 32] : pb[p];
+    }
+
+private:
+    map<char, char> pb;
+};
 
 class Wheels
 {
@@ -55,7 +79,7 @@ public:
 
     }
 
-    //todo 完善函数,插线板暂未实现
+    //todo write functions
     string getCode()
     {
         return new_code;
@@ -69,39 +93,50 @@ public:
         }
     }
 
-    Enigma(string raw_code, int wheels, char p[])
+    void resetWheel(int index, char p)
     {
-        this->raw_code = raw_code;
+        this->wh[index].setWheel(p);
+    }
+
+    Enigma(string r_code, int wheels, char p[], map<char, char> p_point)
+    {
+        this->raw_code = r_code;
         this->wheels = wheels;
-        WheelsInit(p);
+        for (int i = 0; i < wheels; i++)
+        {
+            Wheels temp;
+            temp.setWheel(p[i]);
+            this->wh.push_back(temp);
+        }
+        this->Pb.Init(p_point);
     }
 
 private:
     string raw_code, new_code;
     int wheels;
     vector<Wheels> wh;
+    Plugboard Pb;
 
-    void WheelsInit(char p[])
-    {
-        for (int i = 0; i < wheels; i++)
-        {
-            Wheels temp;
-            temp.setWheel(p[i]);
-            wh.push_back(temp);
-        }
-    }
+
 };
 
 
 int main()
 {
-    int asa;
-    cin >> asa;
-    char op[3] = {};
-    for (int i = 0; i < asa; i++) cin >> op[i];
-    Enigma a("A", 3, op);
+//    int asa;
+//    cin >> asa;
+//    char op[3] = {};
+//    for (int i = 0; i < asa; i++) cin >> op[i];
+//    Enigma a("A", 3, op,p_op);
+//
+//    a.showWheelsSet();
 
-    a.showWheelsSet();
+    map<char, char> p_op;
+    p_op.insert(pair<char, char>('a', 'b'));
+    Plugboard a;
+    a.Init(p_op);
+    cout << a.getLetter('a') << endl;
+
 
     system("pause");
 
